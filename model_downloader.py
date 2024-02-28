74d9dbb98da4e90f2bed4cb569cdfc0b2254a337
holchan <61059652+holchan@users.noreply.github.com>
2024-02-28 04:57:20 -0300

    ads

--- model_downloader.py
+++ model_downloader.py
@@ -1,10 +1,10 @@
 import os
 import subprocess
-import folder_paths, comfy.model_management
+import comfy.sd
 
 class ModelDownloader:
-    @classmethod
-    def INPUT_TYPES(cls):
+    @staticmethod
+    def INPUT_TYPES():
         return {
             "required": {
                 "LINK": ("STRING", {}),
@@ -12,22 +12,18 @@ class ModelDownloader:
             }
         }
 
-    RETURN_TYPES = ("MODEL", "CLIP", "VAE")
-    FUNCTION = "load_checkpoint"
-    CATEGORY = "loaders"
-
-    def load_checkpoint(self, link, output, output_vae=True, output_clip=True):
-        # Download the model file
-        downloaded_file = self.download_model(link, output)
+    @staticmethod
+    def load_checkpoint(LINK, OUTPUT, output_vae=True, output_clip=True):
+        downloaded_file = ModelDownloader.download_model(LINK, OUTPUT)
         if downloaded_file:
-            # Load the downloaded model file
-            out = comfy.sd.load_checkpoint_guess_config(downloaded_file, output_vae=True, output_clip=True, embedding_directory=output)
+            out = comfy.sd.load_checkpoint_guess_config(downloaded_file, output_vae=output_vae, output_clip=output_clip, embedding_directory=OUTPUT)
             return out[:3]
         else:
             print("Error loading checkpoint. Downloaded file not found.")
             return None
 
-    def download_model(self, link, output):
+    @staticmethod
+    def download_model(link, output):
         wget_command = ["wget", link, "-P", output]
         try:
             subprocess.run(wget_command, check=True)
